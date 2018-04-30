3dac73e48ff54958e922836c5d0af085c7b3b0ea
leonardomgt <leogt-15@hotmail.com>
2018-04-30 19:21:51 +0100

    db

--- Treasure Seek Server/src/main/DBOperations.java
+++ Treasure Seek Server/src/main/DBOperations.java
@@ -3,5 +3,5 @@ package main;
 import java.rmi.Remote;
 
 public interface DBOperations extends Remote {
-	String insertUser(String username, String token);
+	String insertUser(String username, String email, String token);
 }
