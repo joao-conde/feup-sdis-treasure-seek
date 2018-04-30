026fd3ce6929c4f72766a6a399ac5d5d31f15bac
joao-conde <joaoconde_13@hotmail.com>
2018-04-30 18:33:33 +0100

    scripts and AppServer start

---
+++ Treasure Seek Server/compile.sh
@@ -0,0 +1,5 @@
+mkdir bin
+cd bin
+rm *.class
+cd ..
+javac -d bin src/*/*.java
---
+++ Treasure Seek Server/src/main/AppServer.java
@@ -0,0 +1,14 @@
+package main;
+
+public class AppServer{
+
+
+    public static void main(String args[]){
+        AppServer server = new AppServer();
+    }
+
+    public AppServer(){
+        System.out.println("---App Server---");
+    }
+
+}
