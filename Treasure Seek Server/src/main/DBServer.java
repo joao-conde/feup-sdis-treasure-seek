c33bb6243df2b99a5fb8137dced0fa38ab704793
leonardomgt <leogt-15@hotmail.com>
2018-05-02 13:50:51 +0100

    SQLite Java Connection

--- Treasure Seek Server/src/main/DBServer.java
+++ Treasure Seek Server/src/main/DBServer.java
@@ -1,17 +1,76 @@
 package main;
 
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBServer implements DBOperations{
 
-	public static void main(String[] args) {
+	private static DBServer instance = null;
+    private static Connection connection = null;
+    private static String DBNAME = "treasureSeekDB.db";
 
-		
+    public static DBServer getInstance() {
+    	if (instance == null)
+    		instance = new DBServer();
+	    return instance;
 	}
+    
+	public static void main(String[] args) throws RemoteException, AlreadyBoundException {
+		
+		instance = new DBServer();
+		DBOperations dbOps = (DBOperations) UnicastRemoteObject.exportObject(instance, 0);
+        
+		int registryPort = args.length > 0 ? Integer.parseInt(args[0]) : 1099;
+		instance = new DBServer();
+
+		Registry registry = LocateRegistry.createRegistry(registryPort);
+		registry.bind("operations", dbOps); 
+		
+		connect();
+
+	}
+	
+	 /**
+     * Connect to a sample database
+     */
+    public static void connect() {
+        try {
+            String url = "jdbc:sqlite:db/" + DBNAME;
+            // create a connection to the database
+            connection = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    
 
 	@Override
-	public String insertUser(String username, String email, String token) {
+	public void insertUser(String username, String email, String token) throws SQLException {
 
-		
-		return null;
+		Statement stmt = connection.createStatement();
+        String sql= "INSERT INTO user (username,email,token) " +
+        			"VALUES (" + username + ", " + email + ", " + token + ");"; 
+        stmt.executeUpdate(sql);
+
+        
 	}
 	
 
