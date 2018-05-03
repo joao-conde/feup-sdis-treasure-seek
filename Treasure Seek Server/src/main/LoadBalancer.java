3d0c55c15f001538fd50ae16e168b0a2cc4ff2f9
joao-conde <joaoconde_13@hotmail.com>
2018-05-03 18:58:24 +0100

    listener of client message

--- Treasure Seek Server/src/main/LoadBalancer.java
+++ Treasure Seek Server/src/main/LoadBalancer.java
@@ -14,59 +14,81 @@ import communications.Message;
 import util.ParseMessageException;
 import util.Utils.Pair;
 
+public class LoadBalancer {
 
-public class LoadBalancer{
-	
 	static int serverPort = 6789;
-	private ArrayList<Pair<String,String>> availableServers = new ArrayList<Pair<String,String>>();
-
-    public static void main(String[] args) throws IOException, ParseMessageException, JSONException{
-    	System.out.println("On Load Balancer");
-        LoadBalancer lb = new LoadBalancer();
-    }
-
-    public LoadBalancer() throws IOException, ParseMessageException, JSONException{
-    	
-	 String clientSentence;
-	 String capitalizedSentence;
-	 availableServers.add(new Pair<String,String>("IP1","60"));
-	 availableServers.add(new Pair<String,String>("IP2","61"));
-	 availableServers.add(new Pair<String,String>("IP3","62"));
-
-	 ServerSocket welcomeSocket = new ServerSocket(serverPort);
 	
-	  while (true) {
-	   Socket connectionSocket = welcomeSocket.accept();
-	   Scanner inFromClient =
-	    new Scanner(new InputStreamReader(connectionSocket.getInputStream()));
-	   DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-	   clientSentence = inFromClient.nextLine();
-	   System.out.println("Received from Client: " + clientSentence);
-	   Message message = Message.parseMessage(clientSentence.getBytes());
-	   
-	   Pair<String, String> serverInfo = selectServer();
-	   outToClient.writeBytes(serverInfo.key + " " + serverInfo.value + '\n');
-	  }
+	private ArrayList<Pair<String, String>> availableServers = new ArrayList<Pair<String, String>>();
+	private ServerSocket welcomeSocket;
+	
+	public static void main(String[] args) throws IOException, ParseMessageException, JSONException {
+		System.out.println("On Load Balancer");
+		LoadBalancer lb = new LoadBalancer();
+	}
 
-    }
-    
-    public Pair<String,String> selectServer(){
-    	
-    	Pair<String, String> server = availableServers.get(0);
-    	
-    	availableServers.remove(0);
-    	availableServers.add(server);
-    	
-		return server;	
-    }
-    
-    public void dispatcher() {
-    	
-//    	executor = Executors.newFixedThreadPool(10);
-//    	
-//    	Runnable appServer = new AppServer();
-//		executor.execute(appServer);
-    	
-    }
-    
+	public LoadBalancer() throws IOException, ParseMessageException, JSONException {
+
+		this.welcomeSocket = new ServerSocket(serverPort);
+
+		availableServers.add(new Pair<String, String>("IP1", "60"));
+		availableServers.add(new Pair<String, String>("IP2", "61"));
+		availableServers.add(new Pair<String, String>("IP3", "62"));
+
+		this.dispatcher();
+		
+
+	}
+
+	public Pair<String, String> selectServer() {
+
+		Pair<String, String> server = availableServers.get(0);
+
+		availableServers.remove(0);
+		availableServers.add(server);
+
+		return server;
+	}
+
+	public void dispatcher() throws IOException, ParseMessageException, JSONException {
+
+		String incomingMsg;
+	
+		while (true) {
+
+			Socket connectionSocket = this.welcomeSocket.accept();
+			
+			Scanner in = new Scanner(new InputStreamReader(connectionSocket.getInputStream()));
+			incomingMsg = in.nextLine();
+			
+			DataOutputStream out = new DataOutputStream(connectionSocket.getOutputStream());
+
+			System.out.println("Received message: " + incomingMsg);
+			Message message = Message.parseMessage(incomingMsg.getBytes());
+			
+			this.handleMessage(message, out);
+			
+		}
+		
+	}
+	
+	
+	public void handleMessage(Message message, DataOutputStream out) throws IOException {
+		
+		Message.MessageType msgType = message.getHeader().getMessageType();
+		
+		switch(msgType) {
+		
+		case RETRIEVE_HOST:
+			Pair<String, String> serverInfo = selectServer();
+			out.writeBytes(serverInfo.key + " " + serverInfo.value + '\n');
+			break;
+			
+		//TODO: NEW SERVER
+			
+		default:
+			break;
+		
+		}
+	}
+
 }
